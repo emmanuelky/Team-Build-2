dbb49e1dc33393fbde78fa9cac5f259d40cff91f
sjomansara <sara.sjoman@hotmail.com>
2021-09-06 15:13:15 +0200

    Update Index.jsx

--- linkedin/src/components/IndexLayout/Index.jsx
+++ linkedin/src/components/IndexLayout/Index.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './index.css'
 import Education from '../Education'
+import ProfileSummary from '../ProfileInfo/ProfileSummary'
+import MyNavbar from '../MyNavbar'
+import MyFooter from '../MyFooter'
 
 
 const Index = () => {
     return (
         <div className='d-flex flex-column align-items-center flex-wrap' >
             <div className='border w-100 text-center'>
-                <div>NavBar</div>
+                <div><MyNavbar /></div>
             </div>
 
             <div className='d-flex my-5' >
                 <div className='flex-column profile m-2 text-center'>
-                    <div className='mb-3 border'>Profile</div>
+                    <div className='mb-3 border'><ProfileSummary /></div>
                     <div className='-3 border'>
                         <Education />
                     </div>
@@ -25,7 +28,7 @@ const Index = () => {
 
 
             <div className='border w-100 text-center fixed-bottom' >
-                <div>Footer</div>
+                <div><MyFooter /></div>
             </div>
         </div>
     )
