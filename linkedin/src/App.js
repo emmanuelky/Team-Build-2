e3d420f9c5931fc0d64ed5386d9fef30cc2738bb
M603-droid <mridulanamdeo@gmail.com>
2021-09-06 14:42:55 +0200

    Navbar

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,12 +1,16 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import MyNavbar from './components/MyNavbar';
+import MyFooter from './components/MyFooter';
+
 function App() {
-  return (
+  return ( <>
     <div className="App">
-
+    <MyNavbar/>
+    <MyFooter/>
     </div>
-  );
-}
+    </> );
 
+  }
 export default App;
