33e4dd2f89039cf912c94afec718f2a24ae68416
sjomansara <sara.sjoman@hotmail.com>
2021-09-06 19:43:44 +0200

    footer styling in progress

--- linkedin/src/components/MyFooter.jsx
+++ linkedin/src/components/MyFooter.jsx
@@ -6,17 +6,11 @@ export default function MyFooter(){
 
     return (
       <Container className= "Footer d-flex justify-content-center mx-auto">
-        
-         
           <Row>
-          
+            <Col md={12}><img className="ml-4 mb-2" id="footerImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/LinkedIn_Logo_2013.svg/1280px-LinkedIn_Logo_2013.svg.png" /></Col>
             <Col xs={12} sm={6} lg={2} className="p-0" >
+              
               <ul>
-              <li>
-                  <a href="#home"><span className= "linked"><b>Linked</b></span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-</svg></a>
-                </li>
                 <li>
                   <a className= "text-muted" href="#home">About</a>
                 </li>
@@ -79,7 +73,7 @@ export default function MyFooter(){
                     <DropdownButton 
                     id="footerFontSize"
                     title="English (English)" 
-                    variant="link" 
+                    variant="outline-secondary" 
                     drop="down">
                         <Dropdown.Item href="#">German</Dropdown.Item>
                         <Dropdown.Item href="#" active>English (English)</Dropdown.Item>
