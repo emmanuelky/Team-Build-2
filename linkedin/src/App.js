bd249c94bd40f5d738c4c13f5138b0ec0e0849da
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-09-06 11:11:06 +0200

    page layout - navbar/footer

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,10 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Index from './components/Index'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Index />
     </div>
   );
 }
