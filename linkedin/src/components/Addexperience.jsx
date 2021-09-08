b9d52c0a335a8ad9ad6945419f08181616e0d4c1
Tarunsai Sadu <tarunsai.rct@gmail.com>
2021-09-08 09:47:59 +0200

    POST experience is working

---
+++ linkedin/src/components/Addexperience.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { useState } from "react";
+import { Form, Col, Button } from "react-bootstrap";
+
+export default function Addexperience() {
+  const [experience, setExperience] = useState({
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        " https://striveschool-api.herokuapp.com/api/profile/:5d925e677360c41e0046d1f/experiences",
+        {
+          method: "POST",
+          body: JSON.stringify(experience),
+          headers: {
+            "content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM2MzlmNjdiZTZjMTAwMTVmOWRiZDQiLCJpYXQiOjE2MzA5NDM3MzUsImV4cCI6MTYzMjE1MzMzNX0.aqatGQ0--T-ZQWZJQeYBJ0q7JsbxuWlScmsooaM_1ZE",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log(experience);
+        alert("experience is added");
+      } else {
+        alert("not saved correctly");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setExperience({
+      role: "",
+      company: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+      area: "",
+    });
+  };
+
+  // useEffect(() => {}, []);
+  console.log(experience);
+
+  const handleInput = (e, propertyName) => {
+    setExperience({
+      ...experience,
+      [propertyName]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <h2>ADD EXPERIENCE</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Row>
+            <Form.Label column lg={2}>
+              Title
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="role"
+                value={experience.role}
+                onChange={(e) => handleInput(e, "role")}
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column lg={2}>
+              company
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="company"
+                value={experience.company}
+                onChange={(e) => handleInput(e, "company")}
+              />
+            </Col>
+          </Form.Row>
+
+          <br />
+          <Form.Row>
+            <Form.Label column lg={2}>
+              start DATE
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="date"
+                placeholder="yyyy-mm-dd"
+                value={experience.startDate}
+                onChange={(e) => handleInput(e, "startDate")}
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column lg={2}>
+              end date
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="date"
+                placeholder="yyyy-mm-dd"
+                value={experience.endDate}
+                onChange={(e) => handleInput(e, "endDate")}
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column lg={2}>
+              Description
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="describe"
+                value={experience.description}
+                onChange={(e) => handleInput(e, "description")}
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Label column lg={2}>
+              area
+            </Form.Label>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="location"
+                value={experience.area}
+                onChange={(e) => handleInput(e, "area")}
+              />
+            </Col>
+          </Form.Row>
+          <Button type="submit" className="my-1">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
---
+++ linkedin/src/components/Modal.jsx
@@ -0,0 +1,46 @@
+import Addexperience from "./Addexperience";
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
+
+function Modalbox() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <div
+        id="eduIconColor"
+        className="align-self-center mt-4 mr-3 d-block"
+        onClick={handleShow}
+      >
+        <i class="fas fa-plus"></i>
+      </div>
+      {/* <Button variant="primary" onClick={handleShow}>
+        +
+      </Button> */}
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Addexperience />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+export default Modalbox;
