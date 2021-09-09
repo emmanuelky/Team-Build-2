3e2cf133b7fe7bf2cfc037408c1331e21251d1fd
sjomansara <sara.sjoman@hotmail.com>
2021-09-09 19:22:27 +0200

    finished footer

--- linkedin/src/components/MyFooter.jsx
+++ linkedin/src/components/MyFooter.jsx
@@ -5,10 +5,10 @@ import { Container, Row, Col, Button, Dropdown, DropdownButton } from "react-boo
 export default function MyFooter(){
 
     return (
-      <Container className= "Footer d-flex justify-content-center mx-auto">
+      <Container className="Footer d-flex justify-content-center mx-auto" style={{ fontWeight: "bold" }}>
           <Row>
             <Col md={12}><img className="ml-4 mb-2" id="footerImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/LinkedIn_Logo_2013.svg/1280px-LinkedIn_Logo_2013.svg.png" /></Col>
-            <Col xs={12} sm={6} lg={2} className="p-0" >
+            <Col md={2} className="p-0" >
               
               <ul>
                 <li>
@@ -26,13 +26,9 @@ export default function MyFooter(){
                 <li>
                   <a className= "text-muted" href="#home">Safety Center</a>
                 </li>
-               
-                
               </ul>
-              
-             
             </Col>
-            <Col xs={12} sm={6} lg={2} className="p-0">
+            <Col md={2} className="p-0">
               <ul>
                 <li>
                   <a className= "text-muted" href="#home">Accessibility</a>
@@ -48,7 +44,7 @@ export default function MyFooter(){
                 </li>
               </ul>
             </Col>
-            <Col xs={12} sm={6} lg={2} className="p-0" >
+            <Col  md={2} className="p-0" >
               <ul>
                 <li>
                   <a className= "text-muted" href="#home">Talent Solutions</a>
@@ -64,24 +60,31 @@ export default function MyFooter(){
                 </li>
               </ul>
             </Col>
-            <Col xs={12} sm={6} lg={2} className="p-0">
-                    <Button className="text-muted" variant="link"><i className="fas fa-question-circle"></i><span id="footerFontSize"> Questions?</span> <br /><span className="text-left Questions">Visit our help center</span></Button>
-                    <Button className="text-muted" variant="link"><i className="fas fa-cog"></i> <span id="footerFontSize">Manage your account and privacy</span> <br /> <span className="Manage"> Go to your settings</span></Button>
-                </Col>
-                <Col xs={12} lg={4} className="dropup-language p-0">
+            <Col  md={4} className="p-0">
+              <ul>
+                <li>
+                  <a className= "text-muted" href="#home"><i className="fas fa-question-circle mr-2"></i> <span style={{ fontSize: "14px" }}>Questions?</span> <br/> <span className="ml-4" style={{ fontWeight: "normal" }}>Visit our Help Center.</span></a>
+                </li>
+                <li>
+                  <a className= "text-muted" href="#home"><i className="fas fa-cog mr-2"></i> <span style={{ color: "#a380ce", fontSize: "14px" }}>Manage your account and privacy</span> <br/> <span className="ml-4" style={{ fontWeight: "normal" }}>Go to your Settings.</span></a>
+                </li>
+              </ul>
+            </Col>
+                <Col  md={2} style={{ fontWeight: "normal" }} className="dropup-language p-0">
                     Select Language
                     <DropdownButton 
                     id="footerFontSize"
                     title="English (English)" 
                     variant="outline-secondary" 
-                    drop="down">
+                    drop="down"
+                    >
                         <Dropdown.Item href="#">German</Dropdown.Item>
                         <Dropdown.Item href="#" active>English (English)</Dropdown.Item>
                         <Dropdown.Item href="#">Swedish</Dropdown.Item>
                     </DropdownButton>
                 </Col>
-                <Col xs={12} className="p-0 copyright">
-                <Button className= "copyright ml-4 text-muted" variant="link" id="footerFontSize">LinkedIn Corporation © 2021</Button>
+                <Col md={12} className="p-0 copyright">
+                  <Button className= "copyright ml-4 text-muted" variant="link" id="footerFontSize">LinkedIn Corporation © 2021</Button>
                 </Col>       
           </Row>
         </Container>
