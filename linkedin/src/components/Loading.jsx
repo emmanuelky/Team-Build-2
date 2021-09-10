4db63a1a3810b28cbcfe080ca2b6bb614cab8912
Tarunsai Sadu <tarunsai.rct@gmail.com>
2021-09-10 16:23:11 +0200

    added loader files to components

---
+++ linkedin/src/components/Loading.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
